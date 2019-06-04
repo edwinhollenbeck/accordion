1106c91028249a1e92647ecd03a536c927dc9de9
Edwin Hollenbeck <edwin.hollenbeck@yahoo.com>
2019-06-03 23:23:19 -0700

    Split into different functions.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import Section from './Section';
 
 class App extends React.Component {
-  static defaultProps = {
-    sections: []
-  };
   state = {
     currentButtonIndex: 0
   };
-  render() {
+
+  static defaultProps = {
+    sections: []
+  };
+
+  handleButtonClick(i) {
+    this.setState({currentButtonIndex: i});
+    this.renderContent();
+  }
+
+  renderButtons() {
     const {sections} = this.props
-    function handleButtonClick(i) {
-      this.setState({currentButtonIndex: i})
-    }
+    return sections.map((section, i) => (
+        <li key={i}>
+          <button key={i} type='button' onClick={() => this.handleButtonClick(i)}>
+            {section.title}
+          </button>
+        </li>
+    ))
+  }
+
+  renderContent() {
+    const currentButton = this.props.sections[this.state.currentButtonIndex]
+    return (
+      <p>{this.props.sections.length && currentButton.content}</p>
+    )
+  }
+
+  render() {
     return (
       <ul>
-        {
-          sections.map((section, i) => {
-            <li key={i}>
-              <button key={i} type='button' onClick={() => handleButtonClick(i)}>{section.title}</button>
-              <p>{section.content}</p>
-            </li>
-          })
-      }
+        {this.renderButtons()}
       </ul>
     );
   }
